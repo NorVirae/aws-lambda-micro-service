ab67ba394330cc709caf57d1e3bcecd216b31a00
NorVirae <norbertmbafrank@gmail.com>
2023-08-13 09:29:05 +0100

    wolfhound setup

---
+++ HotelAdmin.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System.Text;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HotelMan_HotelAdmin
+{
+    public class HotelAdmin
+    {
+        public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse()
+            {
+                Headers = new Dictionary<string, string>(),
+                StatusCode = 200,
+            };
+
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Headers", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS, post");
+
+            var bodyContent = request.IsBase64Encoded? Convert.FromBase64String(request.Body): Encoding.UTF8.GetBytes(request.Body);
+
+
+
+            Console.WriteLine("OK");
+            return response;
+        }
+    }
+}
