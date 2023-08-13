2afbbe35c469b9be9fd4b0221c0a7395e1b5c052
NorVirae <norbertmbafrank@gmail.com>
2023-08-13 18:41:05 +0100

    integrate s3 bucket

--- HotelAdmin.cs
+++ HotelAdmin.cs
@@ -1,6 +1,13 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using HttpMultipartParser;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -8,7 +15,7 @@ namespace HotelMan_HotelAdmin
 {
     public class HotelAdmin
     {
-        public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
         {
             var response = new APIGatewayProxyResponse()
             {
@@ -21,8 +28,47 @@ namespace HotelMan_HotelAdmin
             response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS, post");
 
             var bodyContent = request.IsBase64Encoded? Convert.FromBase64String(request.Body): Encoding.UTF8.GetBytes(request.Body);
+            using var memStream = new MemoryStream(bodyContent);
+            var formData = MultipartFormDataParser.Parse(memStream);
+            var hotelName = formData.GetParameterValue(name:"hotelName");
+            var hotelRating = formData.GetParameterValue(name: "hotelRating");
+            var hotelCity = formData.GetParameterValue(name: "hotelCity");
+            var hotelPrice = formData.GetParameterValue(name: "hotelPrice");
 
+            var file = formData.Files.FirstOrDefault();
+            var filename = file.FileName;
 
+            await using var fileContentStream = new MemoryStream();
+            await file.Data.CopyToAsync(fileContentStream);
+            fileContentStream.Position = 0;
+
+            var userId = formData.GetParameterValue(name: "userId");
+            var idToken = formData.GetParameterValue(name: "idToken");
+
+            var token = new JwtSecurityToken(idToken);
+            
+            var group = token.Claims.FirstOrDefault(x => x.Type == "cognito:groups");
+
+            if (group == null || group.Value != " Admin")
+            {
+                response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                response.Body = JsonSerializer.Serialize( new
+                {
+                    error = "UnAuthorized. Must be member of Admin group"
+                });
+            }
+
+            var region = Environment.GetEnvironmentVariable("AWS_REGION");
+            var buketName = Environment.GetEnvironmentVariable("BUCKET_NAME");
+
+            var client = new AmazonS3Client(RegionEndpoint.GetBySystemName(region));
+            await client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest()
+            {
+                BucketName = buketName,
+                Key = file.FileName,
+                InputStream = fileContentStream,
+                AutoCloseStream = true
+            });
 
             Console.WriteLine("OK");
             return response;
