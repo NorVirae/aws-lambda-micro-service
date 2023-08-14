6df338bdb2e3b7532b6f7d672e225a267b863915
NorVirae <norbertmbafrank@gmail.com>
2023-08-14 18:33:12 +0100

    create hotel model

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace HotelMan_HotelAdmin.Models
 {
-    internal class Hotel
+    [DynamoDBTable("Hotels")]
+    public class Hotel
     {
+        [DynamoDBHashKey("userId")]
+        public string UserId { get; set; }
+
+        [DynamoDBRangeKey("Id")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+        public int Rating { get; set; }
+        public string CityName { get; set; }
+        public string FileName { get; set; }
+
     }
 }
